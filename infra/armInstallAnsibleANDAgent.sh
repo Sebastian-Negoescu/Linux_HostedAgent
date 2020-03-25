4749b3d1264e81dfabbc6f9490b898f32905cbfb
Sebastian-Negoescu <sebastian.negoescu@gmail.com>
2020-03-25 18:05:13 +0200

    Update armInstallAnsibleANDAgent.sh

--- infra/armInstallAnsibleANDAgent.sh
+++ infra/armInstallAnsibleANDAgent.sh
@@ -4,8 +4,11 @@ sudo apt-get update && sudo apt-get install -y libssl-dev libffi-dev python-dev 
 sudo pip install ansible
 
 cd /home/ansible
+echo "randompwd" > .pass
 ansURI="https://github.com/Sebastian-Negoescu/Linux_HostedAgent.git"
 git clone $ansURI
 sudo chown -R ansible:ansible ./Linux_HostedAgent/infra/ansible/vmAgent
 cd Linux_HostedAgent/infra/ansible/vmAgent
-ansible-playbook install.yml -vvv
+echo "txmvg74uf4ja3zjovmosm2fzldtkjhribgeltjzr7ayeeazki2nq" > ./group_vars/all/vault.yml
+ansible-vault encrypt ./group_vars/all/vault.yml --vault-password-file ~/.pass
+ansible-playbook install.yml --vault-password-file ~/.pass -vvv
